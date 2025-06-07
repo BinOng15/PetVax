4e14342348cbbfa93b8d261856eec300dfe67b97
TranVanHieu1 <123tranhieu123@gmail.com>
2025-06-07 20:11:12 +0700

    vet

---
+++ PetVax.Repositories/Repository/VetRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetVax.BusinessObjects.Models;
+using PetVax.Repositories.IRepository;
+using PetVax.Repositories.Repository.BaseResponse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetVax.Repositories.Repository
+{
+    public class VetRepository : GenericRepository<Vet>, IVetRepository
+    {
+        public VetRepository() : base()
+        {
+        }
+        public async Task<int> CreateVetAsync(Vet vet, CancellationToken cancellationToken)
+        {
+            return await CreateAsync(vet, cancellationToken);
+        }
+
+        public Task<bool> DeleteVetAsync(int vetId, CancellationToken cancellationToken)
+        {
+            return DeleteAsync(vetId, cancellationToken);
+        }
+
+        public Task<List<Vet>> GetAllVetsAsync(CancellationToken cancellationToken)
+        {
+            return GetAllAsync(cancellationToken);
+        }
+
+
+
+        public Task<Vet> GetVetByIdAsync(int vetId, CancellationToken cancellationToken)
+        {
+            return _context.Vets.FirstOrDefaultAsync(v => v.VetId == vetId, cancellationToken);
+        }
+
+        public Task<Vet> GetVetByVetCodeAsync(string vetCode, CancellationToken cancellationToken)
+        {
+            return _context.Vets.FirstOrDefaultAsync(v => v.VetCode == vetCode, cancellationToken);
+        }
+
+        public Task<int> UpdateVetAsync(Vet vet, CancellationToken cancellationToken)
+        {
+            return UpdateAsync(vet, cancellationToken);
+        }
+    }
+}
